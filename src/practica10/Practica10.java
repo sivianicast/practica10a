a1238cc45a9e19576a083488bfc5039c7dd6bc90
Siviani <siv_cast_22@yahoo.com>
2019-10-16 17:55:25 -0600

    Version 2

--- src/practica10/Practica10.java
+++ src/practica10/Practica10.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practica10;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author siviany
- */
 public class Practica10 {
 
     /**
@@ -18,12 +9,14 @@ public class Practica10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //esta es la version 2
         String numTodos = "";
         int num = 1;
         while (num <= 100) {
-            numTodos = numTodos+num+"  ";
+            numTodos = numTodos + num + "  ";
             num++;
         }
+
         JOptionPane.showMessageDialog(null, numTodos);
     }
 
